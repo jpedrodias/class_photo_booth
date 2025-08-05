a58f25d6ae4241266584fec3d3ff362c0badfc3b
Pedro Dias <jpedrodias@gmail.com>
2025-08-05 22:13:23 +0100

    Script para iniciar o Docker Compose com o UID/GID correto

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Script para iniciar o Docker Compose com o UID/GID correto
-# Uso: chmod +x ./start.sh && ./start.sh
+# Usar: chmod +x ./start.sh && ./start.sh
 
 echo "=== Iniciando Class Photo Booth ==="
 echo "Usuário atual: $(whoami) (UID: $(id -u), GID: $(id -g))"
@@ -13,7 +13,7 @@ export GID=$(id -g)
 echo "Usando UID=$UID e GID=$GID no container"
 
 # Criar diretórios no host se não existirem
-mkdir -p ./flaskapp/photos_originals ./flaskapp/photos_thumbs ./flaskapp/zips
+mkdir -p ./flaskapp/photos_originals ./flaskapp/photos_thumbs
 
 echo "Parando containers existentes..."
 docker-compose down
