f2ba214a4c583be49463d4c38970a73e2a348c99
Pedro Dias <jpedrodias@gmail.com>
2025-08-12 18:16:25 +0100

    Define variáveis de ambiente necessárias

--- setup_local_env.ps1
+++ setup_local_env.ps1
@@ -8,6 +8,10 @@ $env:FLASK_APP   = "app.py"         # Substitua pelo seu arquivo Flask principal
 $env:FLASK_ENV   = "development"    # Ou "production" se for o
 $env:FLASK_DEBUG = "1"            # Ativar debug mode
 
+$env:POSTGRES_USER = "postgres_user"
+$env:POSTGRES_PASSWORD = "postgres_password"
+$env:POSTGRES_DB = "fotos"
+
 Write-Host "Ambiente Flask configurado:"
 Write-Host "`tMAIL_SENDER = $env:MAIL_SENDER"
 Write-Host "`tFLASK_APP   = $env:FLASK_APP"
