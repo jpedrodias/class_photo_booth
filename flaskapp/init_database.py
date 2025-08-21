325604374aabcd48565bf0b3e542d01a4389c8b5
Pedro Dias <jpedrodias@gmail.com>
2025-08-22 00:01:58 +0100

    inicializar first user

--- flaskapp/init_database.py
+++ flaskapp/init_database.py
@@ -1,14 +1,27 @@
 #!/usr/bin/env python3
 """
 Script para inicializar a base de dados do Class Photo Booth
+
+docker exec -it flaskapp /bin/bash -c "python ./init_database.py"
 """
 
-from app import app, db
+from app import app, db, User
+
 
 if __name__ == '__main__':
     print("Inicializando base de dados...")
     with app.app_context():
         db.create_all()
         print("Base de dados inicializada com sucesso!")
-    print("\nAgora pode executar a aplicação com: python app.py")
-    print("O primeiro utilizador a registar-se receberá permissões de administrador.")
+
+        admin = User()
+        admin.email = 'admin@example.com'
+        admin.name = 'Administrador'
+        admin.password = 'ChangeMe1#'
+        admin.role = 'admin'
+        admin.is_active = True
+        
+        db.session.add(admin)
+        db.session.commit()
+
+    print("Utilizador administrador criado com sucesso!")
